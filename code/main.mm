796e0be5469eb3b864af4a0312cf2e511916170a
Debabrata Mondal <debabrata.js@protonmail.com>
2025-07-08 02:49:45 +0530

    day 1: open window

--- code/main.mm
+++ code/main.mm
@@ -1,6 +1,23 @@
+#include <AppKit/AppKit.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    printf("handmade hero");
-    return 0;
+int main(int argc, const char *argv[]) {
+  NSApplication *app = [NSApplication sharedApplication];
+  NSWindow *window = [[NSWindow alloc]
+      initWithContentRect:NSMakeRect(0, 0, 1280, 720)
+                styleMask:(NSWindowStyleMaskTitled | NSWindowStyleMaskClosable |
+                           NSWindowStyleMaskMiniaturizable |
+                           NSWindowStyleMaskResizable |
+                           NSWindowStyleMaskFullScreen |
+                           NSWindowStyleMaskFullSizeContentView)
+                  backing:NSBackingStoreBuffered
+                    defer:NO];
+
+  [window setBackgroundColor:NSColor.purpleColor];
+  [window setTitle:@"Handmade Hero"];
+  [window makeKeyAndOrderFront:nil];
+
+  [app run];
+  printf("handmade hero");
+  return 0;
 }
