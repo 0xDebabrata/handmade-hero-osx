4feffe82600cdd7176c0b4f9d16d977f51434e7d
Debabrata Mondal <debabrata.js@protonmail.com>
2025-07-21 00:05:21 +0530

    init bitmap image

--- code/main.mm
+++ code/main.mm
@@ -1,10 +1,14 @@
 #include <AppKit/AppKit.h>
+#include <cstdint>
+#include <cstdlib>
 #include <stdio.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
 @end
 
-@implementation AppDelegate
+int
+
+    @implementation AppDelegate
 /* NSApplicationDelegate protocols */
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:
     (NSApplication *)sender {
@@ -18,6 +22,8 @@
 
 /* NSWindowDelegate protocols */
 - (NSSize)windowWillResize:(NSWindow *)sender toSize:(NSSize)frameSize {
+  int width = frameSize.width;
+  int height = frameSize.height;
   return frameSize;
 }
 @end
@@ -40,6 +46,25 @@ int main(int argc, const char *argv[]) {
   [window setBackgroundColor:NSColor.purpleColor];
   [window setTitle:@"Handmade Hero"];
   [window makeKeyAndOrderFront:nil];
+
+  int bitmapHeight = 3;
+  int width = window.contentView.bounds.size.width;
+  int height = window.contentView.bounds.size.height;
+  int pitch = width * height;
+  int bytesPerPixel = 4;
+  uint8_t *buffer = (uint8_t *)malloc(pitch * bitmapHeight);
+
+  NSBitmapImageRep *img = [[[NSBitmapImageRep alloc]
+      initWithBitmapDataPlanes:&buffer
+                    pixelsWide:width
+                    pixelsHigh:height
+                 bitsPerSample:8
+               samplesPerPixel:4
+                      hasAlpha:true
+                      isPlanar:false
+                colorSpaceName:NSDeviceRGBColorSpace
+                   bytesPerRow:pitch
+                  bitsPerPixel:bytesPerPixel * 8] autorelease];
 
   [app run];
   return 0;
